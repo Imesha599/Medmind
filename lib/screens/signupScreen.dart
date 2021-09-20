030a57bfebeeec2de390e35c8e6be7609ce28b18
Nethmin Dulsara <dulsaranethmin@gmail.com>
2021-09-20 14:07:17 +0530

    modify sign up screen

--- lib/screens/signupScreen.dart
+++ lib/screens/signupScreen.dart
@@ -11,12 +11,29 @@ class _SignupScreenState extends State<SignupScreen> {
   TextEditingController _emailcontroller = TextEditingController();
   TextEditingController _passwordcontroller = TextEditingController();
   TextEditingController _telephonecontroller = TextEditingController();
+
+  TextEditingController _gurdian1namecontroller = TextEditingController();
+  TextEditingController _gurdian1telephonecontroller = TextEditingController();
+  TextEditingController _gurdian2namecontroller = TextEditingController();
+  TextEditingController _gurdian2telephonecontroller = TextEditingController();
+  int _index = 0;
   @override
   Widget build(BuildContext context) {
     final mq = MediaQuery.of(context);
     final logo = Image.asset(
       'assets/logo.png',
-      height: mq.size.height / 5,
+      height: mq.size.height / 7,
+    );
+    final logogroup = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Text(
+          'Create Account',
+          style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+        ),
+        logo,
+      ],
     );
     final usernamefield = TextFormField(
       decoration: InputDecoration(
@@ -78,17 +95,101 @@ class _SignupScreenState extends State<SignupScreen> {
       children: [
         usernamefield,
         SizedBox(
-          height: mq.size.height / 28,
+          height: mq.size.height / 30,
         ),
         telephone,
         SizedBox(
-          height: mq.size.height / 28,
+          height: mq.size.height / 30,
         ),
         emailfield,
         SizedBox(
-          height: mq.size.height / 28,
+          height: mq.size.height / 30,
         ),
         passwordfield,
+      ],
+    );
+
+    final gurdian1namefield = TextFormField(
+      decoration: InputDecoration(
+        hintText: 'John doe',
+        hintStyle: TextStyle(color: Colors.grey[600]),
+        labelText: 'Gurdian name - 1',
+        labelStyle: TextStyle(color: Colors.black, fontSize: 23.0),
+        //errorText: 'Ivalid Email',
+        border: OutlineInputBorder(),
+        suffixIcon: Icon(
+          Icons.error,
+        ),
+      ),
+    );
+
+    final gurdian1telephonefield = TextFormField(
+      decoration: InputDecoration(
+        hintText: '077xxxxxxx',
+        hintStyle: TextStyle(color: Colors.grey[600]),
+        labelText: 'Gurdian Telephone - 1',
+        labelStyle: TextStyle(color: Colors.black, fontSize: 23.0),
+        //errorText: 'Ivalid Email',
+        border: OutlineInputBorder(),
+        suffixIcon: Icon(
+          Icons.error,
+        ),
+      ),
+    );
+
+    final gurdian2namefield = TextFormField(
+      decoration: InputDecoration(
+        hintText: 'John doe',
+        hintStyle: TextStyle(color: Colors.grey[600]),
+        labelText: 'Gurdian name - 2',
+        labelStyle: TextStyle(color: Colors.black, fontSize: 23.0),
+        //errorText: 'Ivalid Email',
+        border: OutlineInputBorder(),
+        suffixIcon: Icon(
+          Icons.error,
+        ),
+      ),
+    );
+
+    final gurdian2telephonefield = TextFormField(
+      decoration: InputDecoration(
+        hintText: '077xxxxxxx',
+        hintStyle: TextStyle(color: Colors.grey[600]),
+        labelText: 'Gurdian Telephone - 2',
+        labelStyle: TextStyle(color: Colors.black, fontSize: 23.0),
+        //errorText: 'Ivalid Email',
+        border: OutlineInputBorder(),
+        suffixIcon: Icon(
+          Icons.error,
+        ),
+      ),
+    );
+
+    final gurdian1info = Column(
+      children: [
+        gurdian1namefield,
+        SizedBox(
+          height: mq.size.height / 30,
+        ),
+        gurdian1telephonefield
+      ],
+    );
+    final gurdian2info = Column(
+      children: [
+        gurdian2namefield,
+        SizedBox(
+          height: mq.size.height / 30,
+        ),
+        gurdian2telephonefield
+      ],
+    );
+    final adduser = Column(
+      children: [
+        gurdian1info,
+        Divider(
+          height: mq.size.height / 20,
+        ),
+        gurdian2info,
       ],
     );
 
@@ -128,6 +229,44 @@ class _SignupScreenState extends State<SignupScreen> {
         login,
       ],
     );
+
+    final step = Stepper(
+      currentStep: _index,
+      onStepCancel: () {
+        if (_index > 0) {
+          setState(() {
+            _index -= 1;
+          });
+        }
+      },
+      onStepContinue: () {
+        if (_index <= 0) {
+          setState(() {
+            _index += 1;
+          });
+        }
+      },
+      onStepTapped: (int index) {
+        setState(() {
+          _index = index;
+        });
+      },
+      steps: <Step>[
+        Step(
+            title: const Text('Add User Infomation'),
+            content: Container(
+              alignment: Alignment.centerLeft,
+              child: fields,
+            )),
+        Step(
+            title: Text('Add Gurdian Infomation'),
+            content: Container(
+              alignment: Alignment.centerLeft,
+              child: adduser,
+            )),
+      ],
+    );
+
     return Scaffold(
       backgroundColor: Colors.grey[200],
       body: Form(
@@ -138,10 +277,15 @@ class _SignupScreenState extends State<SignupScreen> {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              logo,
+              logogroup,
+              SizedBox(height: mq.size.height / 25),
+              //fields,
+              //SizedBox(height: mq.size.height / 32),
+              step,
               SizedBox(height: mq.size.height / 15),
-              fields,
-              SizedBox(height: mq.size.height / 15),
+              //adduser,
+              //SizedBox(height: mq.size.height / 15),
+
               buttons
             ],
           ),
